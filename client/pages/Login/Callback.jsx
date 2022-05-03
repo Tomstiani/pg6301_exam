c92c14a025f0b481b95e7610015420abb773fe7b
Tom Stian <toms.ingebretson@gmail.com>
2022-05-04 01:29:41 +0200

    Callback redirects back to homepage after its done

--- client/pages/Login/Callback.jsx
+++ client/pages/Login/Callback.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Callback = () => {
+  const navigate = useNavigate();
   useEffect(() => {
     const { access_token } = Object.fromEntries(
       new URLSearchParams(window.location.hash.substring(1))
@@ -13,9 +15,10 @@ const Callback = () => {
       },
       body: JSON.stringify({ access_token }),
     });
+    navigate("/");
   });
 
-  return <h1>Login Callback</h1>;
+  return <h1>Please wait...</h1>;
 };
 
 export default Callback;
