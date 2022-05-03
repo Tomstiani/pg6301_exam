f00c42ed13331cff923ef80007db78e9a44352a6
Tom Stian <toms.ingebretson@gmail.com>
2022-05-04 00:52:18 +0200

    Login redirects to login with google

--- client/pages/Login.jsx
+++ client/pages/Login.jsx
@@ -1,12 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 
+const fetchJSON = async (url) => {
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
+  return await response.json();
+};
+
 const Login = () => {
+  const [redirectURL, setRedirectURL] = useState();
+
+  useEffect(async () => {
+    const { authorization_endpoint } = await fetchJSON(
+      "https://accounts.google.com/.well-known/openid-configuration"
+    );
+
+    const params = {
+      response_type: "token",
+      client_id:
+        "458398509144-96ll50575hbbb30at7bice2p008ruu1e.apps.googleusercontent.com",
+      scope: "email profile",
+      redirect_uri: `${window.location.origin}/login/callback`,
+    };
+
+    setRedirectURL(authorization_endpoint + "?" + new URLSearchParams(params));
+  }, []);
+
   return (
     <div>
       <Navbar />
       <div className="element-body">
         <h1>Login</h1>
+        <a href={redirectURL}>Go to login</a>
+        <div>{redirectURL}</div>
       </div>
     </div>
   );
