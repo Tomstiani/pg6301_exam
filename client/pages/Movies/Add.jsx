4a20d937b83d656bccb02ae0e7e93a93a11e4162
Tom Stian <toms.ingebretson@gmail.com>
2022-05-03 23:59:03 +0200

    Added new movie form

--- client/pages/Movies/Add.jsx
+++ client/pages/Movies/Add.jsx
@@ -5,7 +5,26 @@ const AddMovie = () => {
   return (
     <div>
       <Navbar />
-      <div>Add movie</div>
+      <div className="element-body">
+        <h1>Add a new movie</h1>
+        <form action="/api/movie/add" method="post">
+          <div className="form-group">
+            <label>
+              Title
+              <input type="text" name="title" />
+            </label>
+            <label>
+              Year released
+              <input type="text" name="release-year" />
+            </label>
+            <label>
+              Plot
+              <textarea name="plot" cols="30" rows="10"></textarea>
+            </label>
+            <button type="submit">Submit movie</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
