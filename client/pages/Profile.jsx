ee611fc297fa2488d865e0be2eb3b7f12db3686a
Tom Stian <toms.ingebretson@gmail.com>
2022-05-05 15:08:03 +0200

    Styling, allows for role change

--- client/pages/Profile.jsx
+++ client/pages/Profile.jsx
@@ -13,11 +13,49 @@ const Profile = () => {
     return <h1>Error: {error.message}</h1>;
   }
 
+  const handleChange = async (e) => {
+    console.log(e.target.value);
+    //disable select while posting changes
+    document.getElementById("role-select").disabled = true;
+
+    //post changes
+    try {
+      await fetch("/api/profile", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          role: e.target.value,
+        }),
+      });
+    } catch (error) {
+      console.log(error);
+    } finally {
+      //enable select after posting changes
+      document.getElementById("role-select").disabled = false;
+    }
+  };
+
   return (
     <>
       <div className="element-body">
-        <h1>Profile</h1>
-        <div>{JSON.stringify(data)}</div>
+        <div className="profile-header">
+          <img src={data.userInfo.picture} />
+          <h1>{data.userInfo.name}</h1>
+        </div>
+        <h2>Email: {data.userInfo.email}</h2>
+        <div>
+          <h2>Edit role:</h2>
+          <select
+            defaultValue={data.userInfo.role}
+            onChange={(e) => handleChange(e)}
+            id="role-select"
+          >
+            <option value="journalist">Journalist</option>
+            <option value="user">User</option>
+          </select>
+        </div>
       </div>
     </>
   );
